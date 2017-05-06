083f1e1060591b524da2d5db76e4116d53d5dff4
Lorenzo Stella <lorenzostella@gmail.com>
2017-05-06 20:39:36 +0200

    fixed stupid error, removed test on linux

--- podds-test.py
+++ podds-test.py
@@ -25,8 +25,8 @@ for args in test_cases.keys():
         p2 = prob_list[k]
         err = abs(p1-p2)/(1+abs(p1))
         if err >= 1e-2:
-            println("test {} failed".format(k))
+            print("test {} failed".format(k))
             sys.exit(1)
 
-println("all tests were succesful")
+print("all tests were succesful")
 sys.exit(0)
